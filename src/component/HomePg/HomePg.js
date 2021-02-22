88789669a6217946bbc5eb54ee45b9ed89ce18fd
Mohnish Lokhande <mohnishlokhande14@gmail.com>
2021-02-22 15:57:07 +0530

    HomeRight

--- src/component/HomePg/HomePg.js
+++ src/component/HomePg/HomePg.js
@@ -7,22 +7,38 @@ export default class HomePg extends Component {
 
     render() {
         return (
-            <div>
+            <div className="cont">
                 <div className="imgH">
                     <div className="grad">
                         <div className="container homemain">
                             <div className="headmain">
-                                
+
                                 <h4 className="hclass">Your Learning Journey Matters to Us</h4>
-                                <hr/>
-                                <h1 className="hmain">Future <br/>achievers <br/>study from <br/>IITians Online</h1>
+                                <hr />
+                                <h1 className="hmain">Future <br />achievers <br />study from <br />IITians Online</h1>
                                 {/* <button class="btn btn-info">
                                     <Link to="/map" className="" >Register For Free </Link>                    
                                 </button>   */}
-                                <br/>
-                                <button type="button" class="btn btn-primary">Register For Free</button> 
+                                <br />
+                                <button type="button" class="regi">Register For Free</button>
                             </div>
-                            
+                            <div className="getstart">
+                                 <h3>Get Started with Electura!</h3>
+                                 <p style={{margin:"5px"}}>
+                                 Get free Counselling from an IITian
+                                 </p>
+                                 <hr />
+                                 <br />
+                                 <label>Your Name</label>
+                                 <input type="text" />
+                                 <br />
+                                 <label>Phone Number</label>
+                                 <input type="text" />
+                                 <br />
+                                 <button className="send" type="submit">Send</button>
+                               
+                            </div>
+
                         </div>
                     </div>
                 </div>
